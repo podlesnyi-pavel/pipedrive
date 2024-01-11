6cbd73c330c69382e26fdd5a3b7cdf34f80f58cb
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2024-01-11 20:09:26 +0300

    add html and css from pipeline

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 'use strict';
 
-const buttonNewJob = document.querySelector('.button--new-job');
-const buttonClose = document.querySelector('.button--close');
-const snake = document.querySelector('.snake');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const snake = modalWrapper.querySelector('.snake');
+const formNewJob = modalWrapper.querySelector('.form-new-job');
 
 function toggleModal() {
-  document
-    .querySelector('.modal-new-job')
-    .classList.toggle('modal-new-job--visible');
+  modalWrapper.classList.toggle('modal-wrapper--visible');
 }
 
-buttonNewJob.addEventListener('click', () => {
+function resetClassListModal() {
+  modalWrapper.classList.remove('modal-wrapper--visible');
+  snake.classList.remove('snake--invisible');
+  formNewJob.classList.remove('form-new-job--visible');
+}
+
+function closeModal() {
+  toggleModal();
+  resetClassListModal();
+}
+
+document.querySelector('.button--new-job').addEventListener('click', () => {
   toggleModal();
 
   setTimeout(() => {
     snake.classList.add('snake--invisible');
-  }, 3000);
+
+    formNewJob.classList.add('form-new-job--visible');
+  }, 1000);
 });
 
-buttonClose.addEventListener('click', () => {
-  toggleModal();
-  snake.classList.remove('snake--invisible');
-});
+modalWrapper
+  .querySelector('.modal-new-job')
+  .addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
+
+modalWrapper.addEventListener('click', closeModal);
+modalWrapper
+  .querySelector('.button--close')
+  .addEventListener('click', closeModal);
+
+modalWrapper
+  .querySelector('.form-new-job')
+  .addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const submitButton = modalWrapper.querySelector('button[type="submit"]');
+
+    submitButton.innerText = 'Request is sent';
+    submitButton.classList.add('form-new-job__button--red');
+
+    const request = await fetch(
+      'https://f0ksiait.pipedrive.com/api/v1/organizations?session_token=58yz_uyzJPg4aHY9sFge49Am28-Fv02DdMueaFQtAsY&strict_mode=true'
+    );
+
+    console.log(request);
+    console.dir(submitButton.innerText);
+  });
